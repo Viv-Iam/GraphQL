4390405a2771026f006bba186dc7d132d666768f
Vivi-Iam <vivian.opondoh@gmail.com>
2019-01-22 19:03:42 +0300

    Update queries

--- server/main.js
+++ server/main.js
@@ -14,6 +14,25 @@ import { Views } from '../imports/api/tasks'
 
 Meteor.startup(() => {
   // code to run on server at startup
+  casual.seed(123);
+  db.sync({ force: true }).then(() => {
+    _.times(10, () => {
+      return AuthorModel.create({
+        firstName: casual.first_name,
+        lastName: casual.last_name,
+      }).then((author) => {
+        return author.createPost({
+          title: `A post by ${author.firstName}`,
+          text: casual.sentences(3),
+        }).then((post) => {
+          return Views.rawCollection().update(
+            { postId: post.id },
+            { views: casual.integeer(0, 100), postId: post.id},
+            { upsert: true})
+        })
+      })
+    })
+  })
 });
 
 const server = new ApolloServer({
